9914fea0df59e6cb14ae0e103cb4d5673a28ee85
Henry Herman <henry@henryeherman.net>
2012-03-18 07:09:54 -0700

    Added Newark & Mouser

--- contentscript.js
+++ contentscript.js
@@ -3,24 +3,52 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-var regex = /reportpartnumber/;
 
-// Test the text of the body element against our regular expression.
+console.log('Circuit Hub Extentsion Being Called');
+var digikey_str = /digikey/
+var mouser_str = /mouser/
+var newark_str = /newark/
 
-if (regex.test(document.body.innerHTML)) {
-  // The regular expression produced a match, so notify the background page.
-  chrome.extension.sendRequest({}, function(response) {});
-  var partCell = document.getElementById('reportpartnumber');  
-  var partInfos = partCell.parentElement.parentElement.getElementsByTagName('th');
-  for(var i=0; i<partInfos.length;i++){
-	if (partInfos[i].innerText == 'Manufacturer Part Number') {	 
-		cell = partInfos[i].parentElement.getElementsByTagName('td')[0];		
-		partid = cell.innerText
-		newcell = '<a href="https://circuithub.com/parts/search?q='+partid+'">'+partid+'</a>';
-		cell.innerHTML = newcell;
-	}
-  }
+
+console.log('URL::'+document.URL);
+if (digikey_str.test(document.URL))
+{
+    console.log('We are on digikey!');
+    // Test the text of the body element against our regular expression.
+    var regex = /reportpartnumber/;
+    if (regex.test(document.body.innerHTML)) {
+      // The regular expression produced a match, so notify the background page.
+      chrome.extension.sendRequest({}, function(response) {});
+      var partCell = document.getElementById('reportpartnumber');  
+      var partInfos = partCell.parentElement.parentElement.getElementsByTagName('th');
+      for(var i=0; i<partInfos.length;i++){
+        if (partInfos[i].innerText == 'Manufacturer Part Number') {	 
+            var cell = partInfos[i].parentElement.getElementsByTagName('td')[0];		
+            var partid = cell.innerText
+            console.log('Found part#'+partid);
+            var newcell = '<a href="https://circuithub.com/parts/search?q='+partid+'">'+partid+'</a>';
+            cell.innerHTML = newcell;
+        }
+      } 
+    } 
     
-} else {
-  // No match was found.
+} else if (mouser_str.test(document.URL)){
+    console.log('We are on Mouser!');
+    var partInfos = document.getElementById('product-desc').getElementsByTagName('td'); 
+    for(var i=0; i<partInfos.length;i++){
+        if (partInfos[i].hasAttribute('itemprop')) {
+            if(partInfos[i].getAttribute('itemprop')=='ProductID') {                   
+               var partid=partInfos[i].innerText;
+               console.log('Found part#'+partid);
+               partInfos[i].innerHTML= '<a href="https://circuithub.com/parts/search?q='+partid+'">'+partInfos[i].innerHTML+'</a>';
+            }
+        }
+    }
+} else if (newark_str.test(document.URL)){
+    console.log('We are on Newark!');
+    partInfos=document.getElementsByClassName('pd_details')[0];
+    cell = partInfos.children[partInfos.children.length-1]
+    var partid=cell.innerText;
+    console.log('Found part#'+partid);
+    cell.innerHTML='<a href="https://circuithub.com/parts/search?q='+partid+'">'+cell.innerHTML+'</a>';
 }
